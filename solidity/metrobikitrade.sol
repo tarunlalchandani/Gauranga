c51a8a7288f1bf93f349e37b4b4a5008b2b04901
tarunlalchandani <tarunlalchandani4236562@gmail.com>
2019-03-16 09:20:47 +0530

    Create metrobikitrade.sol

---
+++ solidity/metrobikitrade.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.5.0;
+
+import "./MetroBiki.sol";
+//conditions under which a reviewer can review...
+
+contract Review is BicycleRegister {
+
+    mapping(address => uint[]) reviewerToBicycle;
+
+    modifier notReviewed(uint _bicycleId) {
+        uint flag = 0;
+        for(uint i = 0; i < reviewerToBicycle[msg.sender].length; i++) {
+            if(reviewerToBicycle[msg.sender][i] == _bicycleId) {
+                flag == 1;
+                break;
+            }
+        }
+        require(flag == 0, "Already review submitted!");
+        _;
+    }
+
+    function upvoteBicycle(uint _bicycleId) public notReviewed(uint) {
+        require(msg.sender != bicycleToOwner[_bicycleId], "Owner can not review!");  
+        Bicycle storage testBicycle = bicycles[_bicycleId];
+        testBicycle.upvoteCount++;
+    }
+
+    function downvoteBicycle(uint _bicycleId) public notReviewed(uint _bicycleId) {  //seee this
+        // require(msg.sender != bicycleToOwner[_bicycleId]);  
+        Bicycle storage testBicycle = bicycles[_bicycleId];
+        testBicycle.downvoteCount++;
+    }
+}
